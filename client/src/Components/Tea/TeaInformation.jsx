e71d61e5ea098d2f92bf38c4fa3e524c306847dd
erik <erik@lenzmann.net>
2023-11-13 13:24:06 +0100

    implemented frontend review

--- client/src/Components/Tea/TeaInformation.jsx
+++ client/src/Components/Tea/TeaInformation.jsx
@@ -3,11 +3,13 @@ import Modal from 'react-modal';
 import Information from '../Information/Information';
 import Close from '../../Assets/close.png';
 import EmptyStar from '../../Assets/star-empty.png'
+import FilledStar from '../../Assets/star-filled.png'
 import { markAsFavourite } from '../../apiService';
 import { useState } from 'react';
+import TeaRatingModal from './TeaRatingModal';
 
 
-const customStyles = {
+const TeaCardStyle = {
   overlay: {
     backgroundColor: 'transparent',
   },
@@ -26,12 +28,18 @@ const customStyles = {
 
 const TeaInformation = ({ tea, setTeaInformationVisible }) => {
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isRateModalOpen, setIsRateModalOpen] = useState(false);
+  const [star, setStar] = useState(false)
   const [rating, setRating] = useState('Rate')
-  
+
   function setFavourite() {
     const token = localStorage.getItem('accessToken')
     markAsFavourite(tea.name, token);
     document.querySelector('.FavouriteTea').innerHTML = `<h4>${tea.name}</h4>`
+  }
+
+  function openRateModal() {
+    setIsRateModalOpen(true);
   }
 
   function close() {
@@ -40,64 +48,69 @@ const TeaInformation = ({ tea, setTeaInformationVisible }) => {
   }
 
   return (
-    <div className='TeaInformation' id={tea._id}>
-      <Modal className={'Page'} style={customStyles} isOpen={isModalOpen} >
-        <div className="Description">
-          <div className="Title">
-            <div className="Start">
-              <h1>{tea.name}</h1>
-              <span>{tea.description}</span>
-            </div>
-            <div className="End">
-              <div className="End-Content">
-                <div className="Rating">
-                  <img src={EmptyStar} alt="Empty" />
-                  <h4>{rating}</h4>
+    <>
+      <div className='TeaInformation' id={tea._id}>
+        <Modal className={'Page'} style={TeaCardStyle} isOpen={isModalOpen} >
+          <div className="Description">
+            <div className="Title">
+              <div className="Start">
+                <h1>{tea.name}</h1>
+                <span>{tea.description}</span>
+              </div>
+              <div className="End">
+                <div className="End-Content">
+                  <div className="Rating" onClick={openRateModal}>
+                    <img src={!star ? EmptyStar : FilledStar} alt="Empty" />
+                    <h4>{rating}</h4>
+                  </div>
+                  <button onClick={setFavourite}>Mark as favourite</button>
+                  <img src={Close} alt="Close" onClick={close} />
                 </div>
-                <button onClick={setFavourite}>Mark as favourite</button>
-                <img src={Close} alt="Close" onClick={close} />
               </div>
             </div>
-          </div>
-          <div className="Information">
-            <div className="Information-Item">
-              <h3>Type:</h3>
-              <Information text={tea.type} />
-            </div>
-            <div className="Information-Item">
-              <h3>Origin:</h3>
-              <Information text={tea.origin} />
-            </div>
-            <div className="Information-Item">
-              <h3>Brew time:</h3>
-              <Information text={`${tea.brewTime} minutes`} />
-            </div>
-            <div className="Information-Item">
-              <h3>Brew temperature:</h3>
-              <Information text={tea.temperature} />
-            </div>
-            {tea.caffeineLevel && (
+            <div className="Information">
               <div className="Information-Item">
-                <h3>Caffeine:</h3>
-                <Information text={tea.caffeineLevel.charAt(0).toUpperCase() + tea.caffeineLevel.slice(1)} />
+                <h3>Type:</h3>
+                <Information text={tea.type} />
               </div>
-            )}
-            <div className="Information-Item">
-              <h3>Main ingredients:</h3>
-              <Information text={tea.mainIngredients} />
-            </div>
-            <div className="Information-Item">
-              <h3>Color description:</h3>
-              <Information text={tea.colorDescription} />
-            </div>
-            <div className="Information-Item">
-              <h3>Taste description:</h3>
-              <Information text={tea.tasteDescription} />
+              <div className="Information-Item">
+                <h3>Origin:</h3>
+                <Information text={tea.origin} />
+              </div>
+              <div className="Information-Item">
+                <h3>Brew time:</h3>
+                <Information text={`${tea.brewTime} minutes`} />
+              </div>
+              <div className="Information-Item">
+                <h3>Brew temperature:</h3>
+                <Information text={tea.temperature} />
+              </div>
+              {tea.caffeineLevel && (
+                <div className="Information-Item">
+                  <h3>Caffeine:</h3>
+                  <Information text={tea.caffeineLevel.charAt(0).toUpperCase() + tea.caffeineLevel.slice(1)} />
+                </div>
+              )}
+              <div className="Information-Item">
+                <h3>Main ingredients:</h3>
+                <Information text={tea.mainIngredients} />
+              </div>
+              <div className="Information-Item">
+                <h3>Color description:</h3>
+                <Information text={tea.colorDescription} />
+              </div>
+              <div className="Information-Item">
+                <h3>Taste description:</h3>
+                <Information text={tea.tasteDescription} />
+              </div>
             </div>
           </div>
-        </div>
-      </Modal>
-    </div>
+        </Modal>
+      </div>
+      {isRateModalOpen && (
+        <TeaRatingModal name={tea.name} setIsRateModalOpen={setIsRateModalOpen} setRating={setRating} setStar={setStar} />
+      )}
+    </>
   )
 }
 
---
+++ client/src/Components/Tea/TeaRatingModal.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import '../../Styles/TeaRatingModal.css';
+import Modal from 'react-modal';
+import Close from '../../Assets/close.png'
+import EmptyStar from '../../Assets/star-empty.png';
+import FilledStar from '../../Assets/star-filled.png';
+
+const RatingStyle = {
+    content: {
+        position: 'fixed',
+        background: 'linear-gradient(-90deg, #969f77, #968f54)',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        height: '125px',
+        width: '400px'
+    }
+};
+
+const TeaRatingModal = ({ name, setIsRateModalOpen, setRating, setStar }) => {
+    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [hoveredRating, setHoveredRating] = useState(null);
+    const [selectedRating, setSelectedRating] = useState(null);
+
+    const handleStarClick = (num) => {
+        setSelectedRating(num);
+        setRating(`${num}/10`);
+        setStar(true);
+        closeModal();
+    };
+
+    const handleStarHover = (num) => {
+        setHoveredRating(num);
+    };
+
+    const handleStarLeave = () => {
+        setHoveredRating(null);
+    };
+
+    function closeModal() {
+        setIsModalOpen(false)
+        setIsRateModalOpen(false);
+    }
+
+    const renderStar = (index) => {
+        const isFilled = index <= (hoveredRating !== null ? hoveredRating : selectedRating);
+
+        return (
+            <div
+                key={index}
+                className="StarWrapper"
+                onClick={() => handleStarClick(index)}
+                onMouseEnter={() => handleStarHover(index)}
+                onMouseLeave={handleStarLeave}
+            >
+                <img
+                    src={isFilled ? FilledStar : EmptyStar}
+                    alt={`Star-${index}`}
+                    className="StarImage"
+                />
+            </div>
+        );
+    };
+
+    return (
+        <Modal style={RatingStyle} isOpen={isModalOpen}>
+            <div className="TeaRatingModal">
+                <div className="Close">
+                    <img src={Close} alt="Close" onClick={closeModal} />
+                </div>
+                <h2>{name}</h2>
+                <div className="Stars">
+                    {[...Array(10).keys()].map((index) => renderStar(index + 1))}
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default TeaRatingModal;
